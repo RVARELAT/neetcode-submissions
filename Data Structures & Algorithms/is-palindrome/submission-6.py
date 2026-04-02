e5fbf0d8fbe726ab1bfc84bab4e1f113b310c5ff
Ricardo Varela Tellez <114198174+RVARELAT@users.noreply.github.com>
2026-04-01 22:52:31 -0700

    Add: is-palindrome - submission-6

---
+++ Data Structures & Algorithms/is-palindrome/submission-6.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        # Convert string to lowercase (handles case-insensitivity)
+        s = s.lower()
+
+        left = 0
+        right = len(s) - 1
+
+        while left < right:
+            # isalnum() checks if a string (or character) is made of only letters and numbers.
+            while left < right and s[left].isalnum() == False:
+                left += 1
+            while left < right and s[right].isalnum() == False:
+                right -= 1
+
+            # chars are no the same
+            if s[left].lower() != s[right].lower():
+                return False
+            
+            left += 1
+            right -= 1
+        
+        return True
